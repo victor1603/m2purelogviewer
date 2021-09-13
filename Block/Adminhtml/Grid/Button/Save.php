c962c7dbcad80b8677ef0068ae8948578bd4cc55
victor <pankovych.v@gmail.com>
2021-09-13 15:20:13 +0300

    add buttons for grid usage

---
+++ Block/Adminhtml/Grid/Button/Save.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace CodeCustom\PureLogViewer\Block\Adminhtml\Grid\Button;
+
+use Magento\Cms\Block\Adminhtml\Page\Edit\GenericButton;
+use Magento\Framework\View\Element\UiComponent\Control\ButtonProviderInterface;
+
+class Save extends GenericButton implements ButtonProviderInterface
+{
+    public function getButtonData()
+    {
+        return [
+            'label' => __('Save'),
+            'class' => 'save primary',
+            'data_attribute' => [
+                'mage-init' => ['button' => ['event' => 'save']],
+                'form-role' => 'save',
+            ],
+            'sort_order' => 90
+        ];
+    }
+
+    public function getSaveUrl()
+    {
+        return $this->getUrl('*/*/save', []) ;
+    }
+}
